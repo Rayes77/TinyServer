3042381baacffd68e38ca0e4742ba0accb0b8005
Rayes <Rayes777@outlook.com>
2020-12-10 14:40:57 +0800

    zz..

--- src/Server.h
+++ src/Server.h
@@ -1,4 +1,15 @@
 #ifndef TINYSERVER_SERVER_H
 #define TINYSERVER_SERVER_H
 
+#include <sys/socket.h>
+#include <netdb.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <iostream>
+
+class Server{
+
+};
+
 #endif //TINYSERVER_SERVER_H
