d0616e8b2d4826945755ee530afa5d9cacfcf1d0
WildCatz77 <2311566266@qq.com>
2021-01-19 13:04:22 +0800

    HelloWorld!

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,5 @@
-#include <vector>
-#include <netinet/in.h>
-#include <bits/socket.h>
-#include <arpa/inet.h>
-#include <assert.h>
-#include <signal.h>
-#include <unistd.h>
 #include <iostream>
-#include <string.h>
-#include "helper.h"
-const int BUFSIZE = 512;
 
-
-int main(int argc,char *argv[]) {
-    helper help;
-    std::cout<<helper::getListenedFd(argv[1]);
+int main(){
+    std::cout<<"Hello World!"<<std::endl;
 }
