5182e55b3e48d93e83c840a8d5b2c1933d1008c1
WildCatz77 <2311566266@qq.com>
2021-02-12 18:13:33 +0800

    eventloopthread

--- src/EventLoopThread.cpp
+++ src/EventLoopThread.cpp
@@ -9,4 +9,29 @@
 
 
 #include "EventLoopThread.h"
+#include "EventLoop.h"
 
+EventLoopThread::EventLoopThread() {
+
+}
+//TODO using lambda
+EventLoop *EventLoopThread::startLoop() {
+    std::thread thread(std::bind(&EventLoopThread::threadFunc,this));
+    {
+        std::unique_lock guard(mutex_);
+        while (loop_ == NULL){
+            cond_.wait(guard);
+        }
+    }
+    return loop_;
+}
+
+void EventLoopThread::threadFunc() {
+    EventLoop loop;
+    {
+        std::lock_guard guard(mutex_);
+        loop_ = &loop;
+        cond_.notify_one();
+    }
+    loop.loop();
+}
--- src/EventLoopThread.h
+++ src/EventLoopThread.h
@@ -9,10 +9,21 @@
 
 #ifndef TINYSERVER_EVENTLOOPTHREAD_H
 #define TINYSERVER_EVENTLOOPTHREAD_H
+#include <condition_variable>
+#include <functional>
+#include <mutex>
+#include <thread>
 
-
+class EventLoop;
 class EventLoopThread {
-
+public:
+    EventLoopThread();
+    EventLoop* startLoop();
+private:
+    std::mutex mutex_;
+    EventLoop* loop_;
+    std::condition_variable cond_;
+    void threadFunc();
 };
 
 
